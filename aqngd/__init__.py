fb09867d149247d50227fad010ae3ba0507e5d61
Adil Berkani <84444063+Aster-99s@users.noreply.github.com>
2025-07-18 15:49:42 +0100

    Delete aqngd directory

--- aqngd/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-from .parametrized_metric_circuits import generate_parametrized_metric_circuits
-from .metric_observables import generate_metric_observables
-from .qngrad import generate_qngd
-from .k_functions import choose_best_k, generate_k_params_list
-from .pub_generators import gradient_pub, expval_pub, metric_pub, k_evals_pub
-from .interpreters import metric_blocks, interpret_gradient
-from .job_one import generate_job_one_tuple, unpack_job_one
-from .job_two import generate_job_two, unpack_job_two
